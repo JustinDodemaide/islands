ec4486d896f8531b864fea168037932c4befe98e
JustinDodemaide <103222511+JustinDodemaide@users.noreply.github.com>
2025-05-28 19:34:57 -0700

    Camera setups redo

--- Room/Room.gd
+++ Room/Room.gd
@@ -14,6 +14,49 @@ var current_setup:String = initial_setup
 var setup_index:int = 0
 var setup_order = ["wide","monitor1","selection_apparatus"]
 
+var setups = {
+	"wide":{
+		"pos":Vector3(0.035,1.1,0.5),
+		"rot":Vector3(-10,0.0,0.0),
+		"up":"island_wide",
+		"down":null,
+		"left":"mission",
+		"right":null,
+	},
+	"island_wide":{
+		"pos":Vector3(0.135,1.104,0.26),
+		"rot":Vector3(0.0,-1.7,0.0),
+		"up":"island",
+		"down":"wide",
+		"left":null,
+		"right":null,
+	},
+	"island":{
+		"pos":Vector3(0.164,0.928,-0.199),
+		"rot":Vector3(0.0,-13.4,0.0),
+		"up":"island_info",
+		"down":"island_wide",
+		"left":"island_selection",
+		"right":null,
+	},
+	"island_selection":{
+		"pos":Vector3(0.164,0.928,-0.199),
+		"rot":Vector3(0.0,-1.7,0.0),
+		"up":"island_info",
+		"down":"island_wide",
+		"left":"mission",
+		"right":"island",
+	},
+	"island_info":{
+		"pos":Vector3(0.162,1.283,-0.167),
+		"rot":Vector3(0.0,-13.1,0.0),
+		"up":null,
+		"down":"island",
+		"left":"island_selection",
+		"right":null,
+	}
+}
+
 const TRANSITION_TIME = 1.5
 
 const CAMERA_POSITIONS = {
@@ -33,40 +76,31 @@ func _ready() -> void:
 	camera.rotation_degrees = CAMERA_ROTATIONS[initial_setup]
 
 func move_camera(where:String) -> void:
-	if !setup_order.has(where):
-		push_error("NO")
-	if !CAMERA_POSITIONS.has(where):
-		return
-	if !CAMERA_ROTATIONS.has(where):
+	if !setups.has(where):
 		return
 	current_setup = where
 	var pos_tween = create_tween()
 	var rot_tween = create_tween()
-	pos_tween.tween_property(camera,"position",CAMERA_POSITIONS[where],TRANSITION_TIME).set_trans(Tween.TRANS_CUBIC)
-	rot_tween.tween_property(camera,"rotation_degrees",CAMERA_ROTATIONS[where],TRANSITION_TIME).set_trans(Tween.TRANS_CUBIC)
+	pos_tween.tween_property(camera,"position", setups[where]["pos"], TRANSITION_TIME).set_trans(Tween.TRANS_CUBIC)
+	rot_tween.tween_property(camera,"rotation_degrees", setups[where]["rot"], TRANSITION_TIME).set_trans(Tween.TRANS_CUBIC)
 	emit_signal("room_camera_moved", where)
 
 func _input(event: InputEvent) -> void:
-	# Special cases
-	if current_setup == "monitor1" and event.is_action_pressed("ui_left"):
-		setup_index = setup_order.find("selection_apparatus")
-		move_camera("selection_apparatus")
-		return
-	if current_setup == "selection_apparatus" and event.is_action_pressed("ui_right"):
-		setup_index = setup_order.find("monitor1")
-		move_camera("monitor1")
-		return
-	
-	# main logic
-	if event.is_action_pressed("ui_up") and setup_index < setup_order.size() - 1:
-		setup_index += 1
-		move_camera(setup_order[setup_index])
-	if event.is_action_pressed("ui_down") and setup_index > 0:
-		setup_index -= 1
-		move_camera(setup_order[setup_index])
+	var direction = ""
+	if event.is_action_pressed("ui_up"):
+		direction = "up"
+	if event.is_action_pressed("ui_down"):
+		direction = "down"
+	if event.is_action_pressed("ui_left"):
+		direction = "left"
+	if event.is_action_pressed("ui_right"):
+		direction = "right"
 		
-	if event.is_action_pressed("E"):
-		pass
+	if direction == "":
+		return
+	if setups[current_setup][direction] == null:
+		return
+	move_camera(setups[current_setup][direction])
 
 
 func _on_island_display_icon_selected(icon: IslandDisplayIcon) -> void:
